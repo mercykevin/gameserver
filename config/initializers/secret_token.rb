43b417d88dccbcc752baa3a781ed0ae52fb159fa
孙 伟 <mercykevin@gmail.com>
2013-09-22 15:54:06 +0800

    添加

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SanguoServer::Application.config.secret_key_base = '02b7f897c6aa13de39cbc0c763ff72453cba62a3339e79fa9bcca05f97bc5966f079844bd49973f1f30a2721b58c45bbd1e3b8816a23f1f253b413f016325fd8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SanguoServer::Application.config.session_store :cookie_store, key: '_sanguo_server_session'
