e6b22ceee0a0fd73995a06918da6d29a07dd54bf
kevin <mercykevin@gmail.com>
2014-03-24 12:32:10 +0800

    设定环境

--- controllers/player.rb
+++ controllers/player.rb
@@ -13,9 +13,12 @@ end
 post '/player/register' do
 	name = request[:req_parames][:name]
 	headimg = request[:req_parames][:headpic]
+	#创建角色
 	ret = Model::Player.register(name,headimg)
 	player = ret[:player]
+	GameLogger.debug("Controllers::Player./player/register:register retcode=#{ret[:retcode]}")
 	if ret[:retcode] == Const::ErrorCode::Ok
+		#设置登录状态
 		Model::Player.setOnline(request[:game_session_id],player[:playerId])
 	end
 	ret.to_json
