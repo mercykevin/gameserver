0c61b30c0fe04fcadcf06e71f2746c66d443c4d6
andy <andy_andy@126.com>
2014-04-22 16:17:30 +0800

    comm_dao 加个方法 ， 尚未用到 ，测试用。

--- daos/common_dao.rb
+++ daos/common_dao.rb
@@ -22,4 +22,40 @@ class CommonDao
 			end
 		end
 	end
+
+	#更新redis，带watch和事务功能
+	#@param [Hash] { string:{k1:val,k2:val} , zset:{ k1:{value,score} ,k2:{value,score} } }
+	#@return [Array]
+	def updateWithSort(entities)
+		if entities and not entities.empty?
+			# use block for thread safe
+			RedisClient.watch(entities.keys) do
+				multiret = RedisClient.multi do |multi|
+					entities.each do |sort,kvs|
+						kvs.each do |k,v|
+							if v == nil
+								multi.del(k)
+							else
+								case sort
+								when "string"
+									multi.set(k,v.to_json)
+								when "zset" 
+									v.each do |value,score|
+										multi.zadd(k,score,value)
+									end
+								when "list"
+								when "hash"
+								when "none"
+								end
+							end
+						end
+					end
+				end
+				if multiret == nil || multiret.empty?
+					#抛出乐观锁异常
+					raise RedisStaleObjectStateException
+				end
+			end
+		end
+	end
 end
