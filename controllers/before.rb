52babb3f4eb207a0235dedae544e885f329168a2
kevin <mercykevin@gmail.com>
2014-04-24 10:56:04 +0800

    记录内存变化

--- controllers/before.rb
+++ controllers/before.rb
@@ -9,9 +9,32 @@ helpers do
 			end
 		end
 	end
+
+	def record_memory_before
+		if File.exist?("/proc/#{Process.pid}/status")
+			process_status = File.open("/proc/#{Process.pid}/status")
+			13.times { process_status.gets }
+			@rss_before_action = process_status.gets.split[1].to_i
+			process_status.close
+		end
+	end
+
+	def record_memory_end
+		if File.exist?("/proc/#{Process.pid}/status")
+		    process_status = File.open("/proc/#{Process.pid}/status")  
+		    13.times { process_status.gets }  
+		    rss_after_action = process_status.gets.split[1].to_i  
+		    process_status.close  
+		    memlogger.info("CONSUME MEMORY: #{rss_after_action - @rss_before_action} \  
+		KB\tNow: #{rss_after_action} KB\t#{request.url}")
+		end  
+	end  
 end
 
 before do
+	#记录内存开始之前的数据
+	record_memory_before
+	#
 	response['X-UA-Compatible'] = "IE=edge,chrome=1"
 	session_id = cookies[:game_session_id]
 	if not session_id
@@ -25,6 +48,11 @@ before do
 	request[:player] = player
 	logger << "before do:session_id = #{session_id},path_info = #{request.path_info} \n"
 	parseReq(request)
+end
+
+after do
+	#记录请求结束时的内存的数据
+	record_memory_end
 end
 
 
